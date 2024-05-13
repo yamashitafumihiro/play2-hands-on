0f5b5712ab2a6bfc68e7faec72b6c4ab309e6ca5
yamashitafumihiro <f.yamashita@jus.co.jp>
2024-05-13 14:05:55 +0900

    list method

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,11 +1,23 @@
 package controllers
 
+import models.Users
 import play.api.mvc.{MessagesAbstractController, MessagesControllerComponents}
+import scalikejdbc.{DB, select, withSQL}
 
 import javax.inject.Inject
 
 class UserController @Inject()(components: MessagesControllerComponents) extends MessagesAbstractController(components){
-  def list = TODO
+  def list = Action { implicit request =>
+    val u = Users.syntax("u")
+
+    DB.readOnly { implicit session =>
+      val users = withSQL {
+        select.from(Users as u).orderBy(u.id.asc)
+      }.map(Users(u.resultName)).list.apply()
+
+      Ok(views.html.user.list(users))
+    }
+  }
   def edit(id: Option[Long]) = TODO
   def create = TODO
   def update = TODO
