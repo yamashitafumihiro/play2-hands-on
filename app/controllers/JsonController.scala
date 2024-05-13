ebbb5e5e6a00d0bcf9db2e0a3e8ae378aace14d3
yamashitafumihiro <f.yamashita@jus.co.jp>
2024-05-13 16:53:36 +0900

    create/update json API

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import models.Users
 import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
-import play.api.libs.json.{Json, Writes, __}
+import play.api.libs.json.{JsError, Json, Reads, Writes, __}
 import play.api.mvc.{AbstractController, ControllerComponents}
 import scalikejdbc.{DB, select, withSQL}
 import JsonController._
@@ -23,8 +23,28 @@ class JsonController @Inject()(components: ControllerComponents)
       Ok(Json.obj("users" -> users))
     }
   }
-  def create = TODO
-  def update = TODO
+  def create = Action(parse.json) { implicit request =>
+    request.body.validate[UserForm].map{ form =>
+      DB.localTx { implicit session =>
+        Users.create(form.name, form.companyId)
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      BadRequest(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+    }
+  }
+  def update = Action(parse.json) { implicit request =>
+    request.body.validate[UserForm].map{ form =>
+      DB.localTx { implicit session =>
+        Users.find(form.id.get).foreach { user =>
+          Users.save(user.copy(name = form.name, companyId = form.companyId))
+        }
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      BadRequest(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+    }
+  }
   def remove(id: Long) = TODO
 }
 
@@ -35,4 +55,12 @@ object JsonController {
     (__ \ "name"     ).write[String] and
     (__ \ "companyId").writeNullable[Int]
     )(unlift(Users.unapply))
+
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  implicit val userFormReads: Reads[UserForm] = (
+    (__ \ "id").readNullable[Long] and
+    (__ \ "name").read[String] and
+    (__ \ "companyId").readNullable[Int]
+    )(UserForm)
 }
